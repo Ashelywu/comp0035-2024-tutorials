d1a4d0ad2e96fa6f51ba38d0e6ce7af237de0e7a
Ashely Wu <zcbekwu@ucl.ac.uk>
2024-10-11 15:21:55 +0100

    practice on importing dataset

--- src/tutorialpkg/mypkg1/mymodule1.py
+++ src/tutorialpkg/mypkg1/mymodule1.py
@@ -1,3 +1,18 @@
+import tutorialpkg
+from tutorialpkg.mypkg2.mymodule2_2 import fetch_user_data
+from tutorialpkg.mypkg2.mymodule2_1 import calculate_area_of_circle
+
+from pathlib import Path
+
+# This script is located in a subfolder so you need to navigate up to the parent (src) and then its parent (project root), then down to the 'data' directory and finally the .csv file
+csv_file = Path(__file__).parent.parent.joinpath('data', 'paralympics_events_raw.csv')
+
+# Check if the file exists
+if csv_file.exists():
+    print(f"CSV file found: {csv_file}")
+else:
+    print("CSV file not found.")
+
 mock_database = {
     1: {'name': 'Alice', 'email': 'alice@example.com', 'age': 30},
     42: {'name': 'Bob', 'email': 'bob@example.com', 'age': 45},
